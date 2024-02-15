e61164af346a62de9af398c9a91a66c7a0842c73
ObjectJosh <joshwong74@gmail.com>
2024-02-14 20:11:27 -0800

    Added eslint, prettier, tsup

---
+++ src/index.ts
@@ -0,0 +1,17 @@
+#!/usr/bin/env node
+
+import { program } from 'commander';
+import { initProject } from './initProject';
+
+program.name('hack4impact').description('CLI to initialize and set up volunteer management systems');
+
+program.command('init').description('Initialize a new volunteer management project').action(initProject);
+
+program.command('add-db').description('Add a database to the project').action(addDatabase);
+
+function addDatabase() {
+    console.log('Adding a database...');
+    // Database setup logic here
+}
+
+program.parse(process.argv);
