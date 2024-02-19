17ea58d51c277471e4f14084177cc874a1fe8acd
ObjectJosh <joshwong74@gmail.com>
2024-02-19 11:55:10 -0800

    Update husky

--- src/commands/init/plugins/husky.ts
+++ src/commands/init/plugins/husky.ts
@@ -1,6 +1,9 @@
 import { Plugin } from 'types/plugin';
 import { NPM } from './shared';
 import config from './config.json';
+import fs from 'fs';
+import path from 'path';
+import { execSync } from 'child_process';
 
 /**
  * Set up husky pre-commit hooks
@@ -10,23 +13,52 @@ import config from './config.json';
 const husky: Plugin = {
     install: (packageJsonAdditions) => {
         NPM.installDev('husky lint-staged');
-        packageJsonAdditions.scripts = packageJsonAdditions.scripts || {};
-        // Add or modify the husky and lint-staged configurations
-        packageJsonAdditions.scripts.prepare = 'husky install';
-
-        packageJsonAdditions.husky = {
-            hooks: {
-                'pre-commit': 'lint-staged',
-            },
-        };
-        const scripts = ['npm run lint'];
-
-        // If prettier is used in the project, make pre-commit hook run prettier as well
-        config.plugins.prettier && scripts.push('npm run prettier');
-        packageJsonAdditions['lint-staged'] = {
-            'src/**': scripts,
-        };
+        createHuskyPreCommitHook();
+        updatePackageJson(packageJsonAdditions);
     },
 };
+
+/* eslint-disable @typescript-eslint/no-explicit-any */
+function updatePackageJson(packageJsonAdditions: any) {
+    packageJsonAdditions.scripts = packageJsonAdditions.scripts || {};
+    // Add or modify the husky and lint-staged configurations
+    packageJsonAdditions.scripts.prepare = 'husky install';
+
+    packageJsonAdditions.husky = {
+        hooks: {
+            'pre-commit': 'lint-staged',
+        },
+    };
+
+    const scripts = ['npm run lint'];
+
+    // If prettier is used in the project, make pre-commit hook run prettier as well
+    config.plugins.prettier && scripts.push('npm run prettier');
+    packageJsonAdditions['lint-staged'] = {
+        'src/**': scripts,
+    };
+}
+
+function createHuskyPreCommitHook() {
+    const huskyDirPath = path.join(process.cwd(), '.husky');
+    const preCommitFilePath = path.join(huskyDirPath, 'pre-commit');
+
+    // Ensure the .husky directory exists
+    if (!fs.existsSync(huskyDirPath)) {
+        fs.mkdirSync(huskyDirPath);
+    }
+
+    // Write the pre-commit hook script
+    fs.writeFileSync(
+        preCommitFilePath,
+        `#!/bin/sh
+  npx lint-staged
+  `,
+        { encoding: 'utf8' }
+    );
+
+    // Set the pre-commit hook as executable
+    execSync(`chmod +x "${preCommitFilePath}"`);
+}
 
 export default husky;
