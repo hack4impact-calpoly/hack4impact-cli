ea26e613d3f49941ef5379d33250e2b0d99500af
ObjectJosh <joshwong74@gmail.com>
2024-02-23 18:06:12 -0800

    Update template file

--- _template/plugins/mongoose/app/api/users/route.ts
+++ _template/plugins/mongoose/app/api/users/route.ts
@@ -1,6 +1,6 @@
 import { NextRequest, NextResponse } from 'next/server';
-import dbConnect from '../../../lib/dbConnect';
-import User from '../../../models/User';
+import dbConnect from '@/lib/dbConnect';
+import User from '@/models/User';
 
 export async function GET() {
     await dbConnect();
